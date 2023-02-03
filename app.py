b3d72511053c6b072bcbf0b41ea73c1b4fe2db49
Eert Hoogerwerf <eert.hoogerwerf@gmail.com>
2023-02-03 19:14:35 +0100

    Added html home page

--- app.py
+++ app.py
@@ -1,22 +1,8 @@
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import signal
 import sys
 from types import FrameType
 
-from flask import Flask
+from flask import Flask, Response, send_from_directory
 
 from utils.logging import logger
 
@@ -24,14 +10,14 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello() -> str:
+def hello() -> Response:
     # Use basic logging with custom fields
     logger.info(logField="custom-entry", arbitraryField="custom-entry")
 
     # https://cloud.google.com/run/docs/logging#correlate-logs
     logger.info("Child logger with trace Id.")
 
-    return "Hello, World 2!"
+    return send_from_directory('static', 'index.html')
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
