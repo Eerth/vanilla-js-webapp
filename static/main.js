572b2ebf5894d1c30e3265eaaa0e7e059ab272de
Eert Hoogerwerf <eert@welbo.eu>
2023-08-08 13:43:32 +0200

    Added ball

--- static/main.js
+++ static/main.js
@@ -1,32 +1,46 @@
 const body = document.querySelector("body")
 const container = document.querySelector(".container")
 const carImage = document.querySelector(".car")
+const ballImage = document.querySelector(".ball")
 const frontLeftWheel = document.querySelector("#front-left-wheel")
 const frontRightWheel = document.querySelector("#front-right-wheel")
 const physicsWorker = new Worker('static/physics.js')
 
 function onMouseMove(e) {
-  physicsWorker.postMessage({ command: 'mouse', x: e.offsetX, y: e.offsetY })
+  physicsWorker.postMessage({ command: 'mouse', x: e.clientX, y: e.clientY })
 }
 
 physicsWorker.addEventListener('message', function (event) {
   // Handle updates received from the Web Worker
   const car = event.data.car
+  const ball = event.data.ball
   // console.log("car", car)
 
   frontLeftWheel.style.rotate = `${car.wheelAngle}rad`
   frontRightWheel.style.rotate = `${car.wheelAngle}rad`
 
-  carImage.style.left = `${car.x - carImage.clientWidth / 2}px`
-  carImage.style.top = `${car.y - carImage.clientHeight / 2}px`
+  ballImage.style.left = `${ball.x}px`
+  ballImage.style.top = `${ball.y}px`
+
+  carImage.style.left = `${car.x}px`
+  carImage.style.top = `${car.y - carImage.clientHeight/2}px`
   carImage.style.rotate = `${car.r}rad`
+
+  ballImage.style.visibility = 'visible'
+  carImage.style.visibility = 'visible'
 })
 
 function onMouseEnter(e) {
+  // Start car in the center of the screen
+  // Car's reference frame is on the rear axle
   physicsWorker.postMessage({
     command: 'start',
-    x: carImage.clientWidth / 2 + 10,
-    y: carImage.clientHeight / 2 + 10
+    carX: body.clientWidth / 2 - carImage.clientWidth / 2,
+    carY: body.clientHeight / 2,
+    carL: carImage.clientWidth,
+    ballX: 3 * body.clientWidth / 4,
+    ballY: body.clientHeight / 2,
+    ballR: ballImage.clientWidth / 2,
   })
 }
 
--- static/physics.js
+++ static/physics.js
@@ -1,62 +1,143 @@
 let stopRequested = false
-let mouseOffset
+let mouse
 
 self.addEventListener('message', function (event) {
   // Handle incoming messages from the main thread
   // For example, start the physics loop when receiving a 'start' message
   if (event.data.command === 'start') {
     stopRequested = false
-    startPhysicsLoop(event.data.x, event.data.y)
+    startPhysicsLoop(event.data)
   }
   else if (event.data.command === 'stop')
     stopRequested = true
   else if (event.data.command === 'mouse')
-    mouseOffset = event.data
+    mouse = event.data
 })
 
-function startPhysicsLoop(x, y) {
+/**
+ * Calculate the angle of the front wheels with respect to the car's body based on the mouse position
+ * @param {Object} car - Car object
+ * @returns {Number} - Angle in radians
+ */
+function getWheelAngle(car) {
+  // console.log("car angle", car.r * 180 / Math.PI)
+
+  let angle = Math.atan2(mouse.y - car.y, mouse.x - car.x) - car.r
+
+  // Wrap angle to [-PI, PI]
+  if (angle > Math.PI)
+    angle -= 2 * Math.PI
+  else if (angle < -Math.PI)
+    angle += 2 * Math.PI
+
+  // Mirror angle if it's more than 90 degrees
+  if (angle > Math.PI / 2)
+    angle = Math.PI - angle
+  else if (angle < -Math.PI / 2)
+    angle = -Math.PI - angle
+
+  // Max wheel angle is 45 degrees
+  return Math.max(Math.min(angle, Math.PI / 4), -Math.PI / 4)
+}
+
+/**
+ * Calculate the acceleration of the car based on the mouse position
+ * @param {Object} car - Car object
+ * @returns {Number} - Speed in m/s
+ */
+function getAcceleration(car) {
+  const mouseDistance = (mouse.x - car.x) * Math.cos(car.r) + (mouse.y - car.y) * Math.sin(car.r)
+
+  // Return 0 if the mouse is on the car
+  if (mouseDistance < 0)
+    return mouseDistance
+  else if (mouseDistance < car.L)
+    return 0
+  else
+    return mouseDistance - car.L
+}
+
+function calculateCarStep(car, dt) {
+  if (!mouse)
+    return car
+
+  car.wheelAngle = getWheelAngle(car)
+  // console.log("wheelAngle", car.wheelAngle * 180 / Math.PI)
+
+  const acceleration = getAcceleration(car)
+  // console.log("acceleration", acceleration)
+  car.speed = 0.99 * car.speed + acceleration * dt
+
+  car.x = car.x + car.speed * Math.cos(car.r) * dt
+  car.y = car.y + car.speed * Math.sin(car.r) * dt
+  car.r = car.r + car.speed * (Math.tan(car.wheelAngle) / car.L) * dt
+
+  return car
+}
+
+function calculateBallStep(car, ball, dt) {
+  // Calculate the ball's position in the car's reference frame
+  const ballX = (ball.x - car.x) * Math.cos(car.r) + (ball.y - car.y) * Math.sin(car.r);
+  const ballY = -(ball.x - car.x) * Math.sin(car.r) + (ball.y - car.y) * Math.cos(car.r);
+
+  // Find the closest point on the car to the ball
+  const closestX = Math.max(Math.min(ballX, car.L), 0);
+  const closestY = Math.max(Math.min(ballY, car.L / 2), -car.L / 2);
+
+  // Detect collision with the car
+  if (Math.sqrt((ballX - closestX) ** 2 + (ballY - closestY) ** 2) < ball.R) {
+    // Calculate the normal vector of the collision
+    const collisionVectorLength = Math.sqrt((ballX - closestX) ** 2 + (ballY - closestY) ** 2);
+    const normalX = (ballX - closestX) / collisionVectorLength;
+    const normalY = (ballY - closestY) / collisionVectorLength;
+
+    // Calculate the ball's velocity in the car's reference frame
+    const ballDX = (ball.dx * Math.cos(car.r) + ball.dy * Math.sin(car.r));
+    const ballDY = (-ball.dx * Math.sin(car.r) + ball.dy * Math.cos(car.r));
+
+    // Incorporate car's speed into the collision response
+    const relativeSpeed = car.speed - (ballDX * normalX + ballDY * normalY);
+    const ballDXAfter = ballDX - 2 * relativeSpeed * normalX;
+    const ballDYAfter = ballDY - 2 * relativeSpeed * normalY;
+
+    // Calculate the ball's velocity in the world's reference frame
+    ball.dx = ballDXAfter * Math.cos(car.r) - ballDYAfter * Math.sin(car.r);
+    ball.dy = ballDXAfter * Math.sin(car.r) + ballDYAfter * Math.cos(car.r);
+
+    // Move the ball outside of the car
+    ball.x = car.x + (closestX + ball.R) * Math.cos(car.r) - closestY * Math.sin(car.r);
+    ball.y = car.y + (closestX + ball.R) * Math.sin(car.r) + closestY * Math.cos(car.r);
+  }
+
+  // Update the ball's position
+  ball.x += ball.dx * dt;
+  ball.y += ball.dy * dt;
+
+  return ball;
+}
+
+function startPhysicsLoop({carX, carY, carL, ballX, ballY, ballR}) {
   // Initialize variables and setup physics loop
   let t0
   let dt = 0
 
-  const car = { x, y, r: 0, speed: 0, wheelAngle: 0 }
+  // Initialize the car's state
+  let car = { x: carX, y: carY, r: 0, speed: 0, wheelAngle: 0, L: carL }
+  let ball = { x: ballX, y: ballY, dx: 0, dy: 0, R: ballR }
 
-  // Run the physics loop
   function physicsLoop(t) {
-    // Perform physics calculations
     if (t0)
       dt = (t - t0) / 1000
 
     t0 = t
 
-    if (mouseOffset) {
-      // Calculate the angle of the front wheels
-      const angle = Math.atan2(mouseOffset.y - car.y, mouseOffset.x - car.x)
-      
-      // Max wheel angle is 45 degrees
-      car.wheelAngle = Math.max(Math.min(angle, Math.PI / 4), -Math.PI / 4)
+    // Calculate the next state of the car
+    car = calculateCarStep(car, dt)
 
-      // Difference between the car angle and the wheel angle
-      const angleDiff = car.wheelAngle - car.r
+    ball = calculateBallStep(car, ball, dt)
 
-      // Transform the mouse position to the car's coordinate system
-      const mouseOffsetCar = {
-        x: (mouseOffset.x - car.x) * Math.cos(car.r) - (mouseOffset.y - car.y) * Math.sin(car.r),
-        y: (mouseOffset.x - car.x) * Math.sin(car.r) + (mouseOffset.y - car.y) * Math.cos(car.r)
-      }
-
-      car.speed = 0.99 * car.speed + 0.1 * mouseOffsetCar.x * dt
-
-      // The car moves in the direction of the wheels
-      car.r = car.r + 0.1 * car.speed * angleDiff * dt
-      car.x = car.x + car.speed * Math.cos(car.wheelAngle) * dt
-      car.y = car.y + car.speed * Math.sin(car.wheelAngle) * dt
-
-      fps = 1 / dt
-
-      // Send updates back to the main thread
-      self.postMessage({ car, fps })
-    }
+    // Send updates back to the main thread
+    self.postMessage({ car, ball })
 
     if (!stopRequested) {
       // Schedule the next iteration of the physics loop
