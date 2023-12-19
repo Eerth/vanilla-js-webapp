7fa1b6ca5702f7e5dfd01cfc5fab2291a2520891
Eert Hoogerwerf <eert.hoogerwerf@gmail.com>
2023-12-19 12:01:21 +0100

    Removed unused files

--- app.py
+++ app.py
@@ -1,43 +1,12 @@
-import signal
-import sys
-from types import FrameType
-
 from flask import Flask, Response, send_from_directory
-
-from utils.logging import logger
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello() -> Response:
-    # Use basic logging with custom fields
-    logger.info(logField="custom-entry", arbitraryField="custom-entry")
-
-    # https://cloud.google.com/run/docs/logging#correlate-logs
-    logger.info("Child logger with trace Id.")
-
-    return send_from_directory('static', 'index.html')
-
-
-def shutdown_handler(signal_int: int, frame: FrameType) -> None:
-    logger.info(f"Caught Signal {signal.strsignal(signal_int)}")
-
-    from utils.logging import flush
-
-    flush()
-
-    # Safely exit program
-    sys.exit(0)
+    return send_from_directory("static", "index.html")
 
 
 if __name__ == "__main__":
-    # Running application locally, outside of a Google Cloud Environment
-
-    # handles Ctrl-C termination
-    signal.signal(signal.SIGINT, shutdown_handler)
-
     app.run(host="localhost", port=8080, debug=True)
-else:
-    # handles Cloud Run container termination
-    signal.signal(signal.SIGTERM, shutdown_handler)
